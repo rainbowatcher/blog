4d5d1fdb9ea2183251589ef2cc1f7318d3e5336f
rainbowatcher <rainbow-w@qq.com>
2022-10-26 12:01:44 +0800

    chore: clean component

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -6,15 +6,17 @@
     <RouterLink to="/">
       <h1>Polymorph</h1>
     </RouterLink>
-
-    <!-- <svg src="src/public/favicon.svg" /> -->
     <Nav />
   </header>
 </template>
 
 <style scoped>
 .header {
+  z-index: 99;
   -webkit-backdrop-filter: saturate(50%) blur(8px);
   backdrop-filter: saturate(50%) blur(8px);
+}
+h1{
+  @apply text-2xl;
 }
 </style>
