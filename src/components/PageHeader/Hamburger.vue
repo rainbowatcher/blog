91c34683c6d156928cf00be9bc39ec60bec77911
rainbowatcher <rainbow-w@qq.com>
2022-12-15 17:07:44 +0800

    perf: a11y improve

--- src/components/PageHeader/Hamburger.vue
+++ src/components/PageHeader/Hamburger.vue
@@ -15,7 +15,7 @@ const togglePanel = () => {
 
 <template>
   <div md:hidden>
-    <button class="i-carbon-menu" z2 pointer-events-auto @click="togglePanel()" />
+    <button class="i-carbon-menu" z2 pointer-events-auto title="menu" @click="togglePanel()" />
     <div
       v-show="show"
       class="hamburger fixed top-74px left-0 right-0 overflow-auto h-screen w-full bg-zinc-200 color-zinc-900 dark:color-inherit dark:bg-zinc-800 p-4"
