9aed3772a58d2a54ce3eec3933ac368cd1603f5a
rainbowatcher <rainbow-w@qq.com>
2023-06-09 17:18:40 +0800

    chore: apply eslint rule, and add unit test for position.ts

--- src/types/position.ts
+++ src/types/position.ts
@@ -1,38 +1,75 @@
-// import { resolveUnref } from "@vueuse/core"
-// import type { ComputedRef } from "vue"
-export type LengthUnit = "px" | "rem" | "em"
-const lengthRE = /^\s*([+-]?[\d\.]*)\s*(.*)\s*$/i
+const lengthUnits = ["px", "rem", "em"]
+export type LengthUnit = typeof lengthUnits[number]
+const lengthRE = /^-?(\d+(?:\.\d*)?|\.\d+)(px|rem|em)?$/i
 
 export class Length {
-  public size: number
-  public unit: LengthUnit
-
-  public constructor(size: number, unit: LengthUnit) {
-    this.size = size
-    this.unit = unit
-  }
-
-  public static valueOf(length: string) {
-    if (!length || length.length === 1) {
-      return new Error("length must greater than 1")
+  public static valueOf(length?: string): Length | undefined {
+    if (!length) {
+      return undefined
     }
+
     const match = lengthRE.exec(length)
     if (!match) {
-      return new Error("invalid length")
+      return undefined
     }
+
     const size = Number(match[1])
-    const unit = match[2] as LengthUnit
-    if (isNaN(size)) {
-      return new Error("can't parse size")
+    const unit = match[2] || "px"
+    if (isNaN(size) || !lengthUnits.includes(unit)) {
+      return undefined
     }
 
     return new Length(size, unit)
   }
+
+  public size: number
+  public unit: LengthUnit
+
+  constructor(size: number, unit: LengthUnit) {
+    this.size = size
+    this.unit = unit
+  }
 }
 
-export interface Position {
+export type Position = {
   top?: Length
   bottom?: Length
   left?: Length
   right?: Length
+}
+
+
+if (import.meta.vitest) {
+  const { it, expect, describe } = import.meta.vitest
+  describe("Length", () => {
+    describe("valueOf", () => {
+      it("should return undefined for empty string", () => {
+        expect(Length.valueOf("")).toBeUndefined()
+      })
+
+      it("should return undefined for invalid length string", () => {
+        expect(Length.valueOf("abc")).toBeUndefined()
+        expect(Length.valueOf("10abc")).toBeUndefined()
+        expect(Length.valueOf("10 ")).toBeUndefined()
+      })
+
+      it("should return undefined for invalid unit", () => {
+        expect(Length.valueOf("10mm")).toBeUndefined()
+      })
+
+      it("should return Length object for valid length string", () => {
+        expect(Length.valueOf("10px")).toEqual(new Length(10, "px"))
+        expect(Length.valueOf("10rem")).toEqual(new Length(10, "rem"))
+        expect(Length.valueOf("2.5px")).toEqual(new Length(2.5, "px"))
+        expect(Length.valueOf(".5em")).toEqual(new Length(0.5, "em"))
+        expect(Length.valueOf("-0.5em")).toEqual(new Length(-0.5, "em"))
+      })
+
+      it("should return Length object with default unit if unit is not provided", () => {
+        expect(Length.valueOf("10")).toEqual(new Length(10, "px"))
+        expect(Length.valueOf("2.5")).toEqual(new Length(2.5, "px"))
+      })
+    })
+  })
+
 }
