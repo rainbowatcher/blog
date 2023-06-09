02e43560a9cf1b184fb22465c6b6a559a96a2856
rainbowatcher <rainbow-w@qq.com>
2023-06-09 17:17:19 +0800

    docs: add comments for useAsidePos, useFontSize

--- src/composables/usePosition.ts
+++ src/composables/usePosition.ts
@@ -6,6 +6,9 @@ import { Length } from "~/types/position"
 export const pageHeaderHight = useCssVar("--sika-h-page-header")
 export const postHeaderHight = useCssVar("--sika-h-post-header")
 
+/**
+ * hook that handles the positioning of an aside element
+ */
 export function useAsidePos() {
   const asideClass = ref("is-abs")
 
@@ -21,6 +24,10 @@ export function useAsidePos() {
     }
   }
 
+  onMounted(() => {
+    onScroll()
+  })
+
   const canceler = useEventListener("scroll", onScroll)
 
   return {
@@ -34,7 +41,7 @@ export function usePx(length: MaybeComputedRef<string>, element?: MaybeElementRe
   const elementRef = computed(() => unrefElement(element) ?? computedBody.value)
 
   if (elementRef && length) {
-    const fontSize = useFontSize(unrefElement(elementRef))
+    const fontSize = useFontSize(unrefElement(elementRef) as HTMLElement | undefined)
     let unit: LengthUnit = "px"
     let size = -1
 
@@ -56,10 +63,19 @@ export function usePx(length: MaybeComputedRef<string>, element?: MaybeElementRe
   return px
 }
 
-export function useFontSize(el?: MaybeElementRef<HTMLElement | null>, defaultSize = 16) {
-  let fontSize: number = defaultSize
-  if (isClient && unrefElement(el)) {
-    fontSize = Number.parseInt(window.getComputedStyle(unrefElement(el)!).getPropertyValue("font-size"))
+/**
+ * Custom hook that retrieves the font size of a given element or returns a default value.
+ *
+ * @param maybeElRef Optional reference to the element whose font size is to be retrieved.
+ * @param defaultSize The default font size to be returned if no element is provided or if running on the server-side.
+ * @returns The font size of the element, if available and running on the client-side; otherwise, it returns the default font size.
+ */
+export function useFontSize(maybeElRef?: MaybeElementRef<HTMLElement | undefined>, defaultSize = 16) {
+  const el = unrefElement(maybeElRef)
+  if (isClient && el) {
+    const fontSize = window.getComputedStyle(el).getPropertyValue("font-size")
+    return parseInt(fontSize, 10)
   }
-  return fontSize
+
+  return defaultSize
 }
