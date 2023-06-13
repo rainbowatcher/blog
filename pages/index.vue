f88584dc8b0724edf5cd6b9d6a949ef80c7f1dc6
rainbowatcher <rainbow-w@qq.com>
2023-06-13 22:51:24 +0800

    chore: add props in Postlist

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
+<script lang="ts" setup>
+import { usePages } from "../src/composables/use-pages"
+
+const pages = usePages()
+</script>
+
 <template>
   <HomeLayout>
-    <PostList />
+    <PostList :pages="pages" />
   </HomeLayout>
 </template>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,16 +1,9 @@
 <script lang="ts" setup>
-const router = useRouter()
-const pages = router.getRoutes()
-  .filter(i => i.path.startsWith("/posts") && !i.meta.frontmatter.hide)
-  .sort((a, b) => +new Date(b.meta.frontmatter.date) - +new Date(a.meta.frontmatter.date))
-  .map(i => ({
-    link: i.path,
-    title: i.meta.frontmatter.title || "Untitled",
-    excerpt: i.meta.frontmatter.excerpt,
-    // headerImage: i.meta.frontmatter.headerImage,
-    tags: i.meta.frontmatter.tags,
-    date: i.meta.frontmatter.date ? useDateFormat(i.meta.frontmatter.date, "YYYY-MM-DD").value : "",
-  }))
+import { PageInfo } from "~/composables/use-pages"
+
+defineProps<{
+  pages: PageInfo
+}>()
 </script>
 
 <template>
