9af5282c281eac054c0a846ee51f22e142804974
rainbowatcher <rainbow-w@qq.com>
2023-06-13 22:52:40 +0800

    refactor: refactor the tag page

---
+++ pages/tags/[name].vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { useRoute } from "vue-router"
+
+const route = useRoute()
+console.log(route.params.name)
+</script>
+
+<template>
+  {{ "hello" }}
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/tags/index.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import PostList from "../../src/components/PostList.vue"
+import { useTags } from "../../src/composables/tags"
+import { usePages } from "../../src/composables/use-pages"
+
+const pages = usePages()
+const tags = useTags()
+</script>
+
+<template>
+  <PostLayout>
+    <main flex="~ col">
+      <div max-w-4xl mxa my-8>
+        <span
+          bg-blue text-black
+          inline-block cursor-pointer outline="zinc-300 dark:zinc-600 solid 1"
+          rounded-lg mr-2 px-2 mb-2 hover="bg-sky text-zinc-800 outline-none"
+        >Show All</span>
+        <span
+          v-for="i in tags" :key="i.name" inline-block cursor-pointer outline="zinc-300 dark:zinc-600 solid 1"
+          rounded-lg mr-2 px-2 mb-2 hover="bg-sky text-zinc-800 outline-none"
+        >
+          {{ i.name }}<sup>{{ i.pages.length }}</sup>
+        </span>
+      </div>
+      <PostList :pages="pages" />
+    </main>
+  </PostLayout>
+</template>
+
+<style scoped>
+
+</style>
