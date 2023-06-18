2ce925816edf0ec779b15f6e524fe113660dc792
rainbowatcher <rainbow-w@qq.com>
2023-06-18 23:10:03 +0800

    refactor: rename pageheader to pagenav

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -5,16 +5,7 @@ const randomColors = Array.from({ length: 5 }, _ => randomColor("rgba"))
 </script>
 
 <template>
-  <PageHeader />
-  <div class="home-page-header">
-    <span>Rainbow Watcher</span>
-    <!-- <div class="ma h-full max-w-4xl py-8rem text-center">
-      <div class="relative h-8 inline-flex items-stretch bg-neutral8/80 vertical-middle lh-8">
-        <Dropdown />
-        <input type="test" class="h-8 max-w-md border-(y-1 r-0 sky600) bg-transparent p-0 lh-8 focus:outline-none" autocomplete="off" placeholder="type" title="searchInput">
-        <button class="m0 cursor-pointer border-(y-1 l-0 r-1 sky500) rounded-r bg-sky/80 p0 px-4 py-1 lh-4" title="search">
-          search
-        </button>
+  <PageNav />
       </div>
     </div> -->
   </div>
