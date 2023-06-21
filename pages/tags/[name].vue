41421db83421c88e9d0dd1e0c238af18eb16b177
rainbowatcher <rainbow-w@qq.com>
2023-06-21 09:49:26 +0800

    feat: extract a component for the list of tags.

--- pages/tags/[name].vue
+++ pages/tags/[name].vue
@@ -9,17 +9,7 @@ const currentTag = computed(() => tags.find(i => i.name.toLowerCase() === props.
 
 <template>
   <section flex="~ col">
-    <div mxa mt-8 max-w-4xl px4 text-balance>
-      <Tag
-        label="Show All" type="success" mb16px mr-2 transition-shadow-300 hover="shadow-[2px_1px_8px_2px_rgb(15,23,42,0.3)] dark:shadow-[2px_1px_8px_2px_rgb(100,116,139,.3)]"
-        @click="$router.push('/tags')"
-      />
-      <Tag
-        v-for="i in tags" :key="i.name" :label="i.name" :sup="i.pages.length"
-        mb4 mr-2 type="info" transition-shadow-300
-        hover="shadow-[2px_1px_8px_2px_rgba(0,0,0,0.3)] dark:shadow-[2px_1px_8px_2px_rgba(163,163,163,.3)]"
-      />
-    </div>
+    <TagList :tags="tags" />
     <p v-if="!currentTag?.pages">
       Empty
     </p>
--- pages/tags/index.vue
+++ pages/tags/index.vue
@@ -8,17 +8,7 @@ const pages = usePages()
 
 <template>
   <section flex="~ col">
-    <div flex="~ row wrap" mt-6 gap-4 px4>
-      <Tag
-        label="Show All" type="success" transition-shadow-300 hover="shadow-[2px_1px_8px_2px_rgb(15,23,42,0.3)] dark:shadow-[2px_1px_8px_2px_rgb(100,116,139,.3)]"
-        @click="$router.push('/tags')"
-      />
-      <Tag
-        v-for="i in tags" :key="i.name" :label="i.name" :sup="i.pages.length"
-        type="info" transition-shadow-300
-        hover="shadow-[2px_1px_8px_2px_rgba(0,0,0,0.3)] dark:shadow-[2px_1px_8px_2px_rgba(163,163,163,.3)]"
-      />
-    </div>
+    <TagList :tags="tags" />
     <PostList :pages="pages" />
   </section>
 </template>
