fbe0134695775c1ac26f513a2d038dbaffbecd50
rainbowatcher <rainbow-w@qq.com>
2023-06-29 10:51:20 +0800

    fix: fix animation height calculate

--- src/composables/details.ts
+++ src/composables/details.ts
@@ -2,106 +2,110 @@ import { isClient } from "@vueuse/core"
 
 // modified from https://css-tricks.com/how-to-animate-the-details-element/
 export function setupDetailsAnimation() {
-  if (isClient) {
-    const allDetails = document.querySelectorAll(".details.custom-block") satisfies NodeListOf<HTMLDetailsElement>
-    allDetails.forEach((el) => {
-      const summary = el.querySelector("summary")
-      const content = el.querySelector(".content") as HTMLElement
-      const closeHeight = ref(0)
-      const animation = ref<Animation>()
-      const isClosing = ref(false)
-      const isExpanding = ref(false)
+  setTimeout(() => {
+    if (isClient) {
+      const allDetails = document.querySelectorAll(".details.custom-block") satisfies NodeListOf<HTMLDetailsElement>
+      allDetails.forEach((el) => {
+        const summary = el.querySelector("summary")
+        const content = el.querySelector(".content") as HTMLElement
+        const closeHeight = ref(0)
+        const animation = ref<Animation>()
+        const isClosing = ref(false)
+        const isExpanding = ref(false)
 
-      summary?.addEventListener("click", (e) => { onClick(e, el) })
+        summary?.addEventListener("click", (e) => { onClick(e, el) })
 
-      function onClick(e :Event, el:HTMLDetailsElement) {
-        e.preventDefault()
+        function onClick(e :Event, el:HTMLDetailsElement) {
+          e.preventDefault()
 
-        el.style.overflow = "hidden"
-        if (isClosing.value || !el.open) {
-          open()
-        } else if (isExpanding.value || el.open) {
-          shrink()
+          el.style.overflow = "hidden"
+          if (isClosing.value || !el.open) {
+            open()
+          } else if (isExpanding.value || el.open) {
+            shrink()
+          }
         }
-      }
 
-      function shrink() {
-        // Set the element as "being closed"
-        isClosing.value = true
+        function shrink() {
+          // Set the element as "being closed"
+          isClosing.value = true
 
-        // Store the current height of the element
-        const startHeight = `${el.offsetHeight}px`
-        // Calculate the height of the summary
-        const endHeight = `${closeHeight.value}px`
+          // Store the current height of the element
+          const startHeight = `${el.offsetHeight}px`
+          // Calculate the height of the summary
+          const endHeight = `${closeHeight.value}px`
+          // console.log({ startHeight, endHeight, firstTop: firstTop.value, lastBottom: lastBottom.value })
 
-        animation.value?.cancel()
+          animation.value?.cancel()
 
-        // Start a WAAPI animation
-        animation.value = el.animate({
-          // Set the keyframes from the startHeight to endHeight
-          height: [startHeight, endHeight],
-        }, {
-          duration: 400,
-          easing: "ease-out",
-        })
+          // Start a WAAPI animation
+          animation.value = el.animate({
+            // Set the keyframes from the startHeight to endHeight
+            height: [startHeight, endHeight],
+          }, {
+            duration: 400,
+            easing: "ease-out",
+          })
 
-        // When the animation is complete, call onAnimationFinish()
-        animation.value.onfinish = () => { onAnimationFinish(false) }
-        // If the animation is cancelled, isClosing variable is set to false
-        animation.value.oncancel = () => isClosing.value = false
-      }
+          // When the animation is complete, call onAnimationFinish()
+          animation.value.onfinish = () => { onAnimationFinish(false) }
+          // If the animation is cancelled, isClosing variable is set to false
+          animation.value.oncancel = () => isClosing.value = false
+        }
 
-      function open() {
-        // Apply a fixed height on the element
-        el.style.height = `${el.offsetHeight}px`
-        closeHeight.value = el.offsetHeight
-        // Force the [open] attribute on the details element
-        el.open = true
-        // Wait for the next frame to call the expand function
-        window.requestAnimationFrame(() => { expand() })
-      }
+        function open() {
+          // Apply a fixed height on the element
+          el.style.height = `${el.offsetHeight}px`
+          closeHeight.value = el.offsetHeight
+          // Force the [open] attribute on the details element
+          el.open = true
+          // Wait for the next frame to call the expand function
+          window.requestAnimationFrame(() => { expand() })
+        }
 
-      function expand() {
-        // Set the element as "being expanding"
-        isExpanding.value = true
-        // Get the current fixed height of the element
-        const startHeight = `${el.offsetHeight}px`
-        // Calculate the open height of the element (content height + details offset height)
-        const endHeight = `${(content?.offsetHeight ?? 0) + el.offsetHeight}px`
-        console.log({ startHeight, endHeight })
+        function expand() {
+          // Set the element as "being expanding"
+          isExpanding.value = true
+          // Get the current fixed height of the element
+          const startHeight = `${el.offsetHeight}px`
+          // Calculate the open height of the element (details offset height + content height + first element margin top + last element margin bottom - 8)
+          const firstTop = el.querySelector(".content > *:first-child")?.computedStyleMap().get("margin-top") as CSSUnitValue
+          const lastBottom = el.querySelector(".content > *:last-child")?.computedStyleMap().get("margin-bottom") as CSSUnitValue
+          const endHeight = `${(content?.offsetHeight ?? 0) + el.offsetHeight + firstTop.value + lastBottom.value - (firstTop.value ? 8 : 0)}px`
+          // console.log({ startHeight, endHeight, firstTop: firstTop.value, lastBottom: lastBottom.value })
 
-        // If there is already an animation running
-        // Cancel the current animation
-        animation.value?.cancel()
+          // If there is already an animation running
+          // Cancel the current animation
+          animation.value?.cancel()
 
-        // Start a WAAPI animation
-        animation.value = el.animate({
-          // Set the keyframes from the startHeight to endHeight
-          height: [startHeight, endHeight],
-        }, {
-          duration: 400,
-          easing: "ease-in-out",
-        })
-        // When the animation is complete, call onAnimationFinish()
-        animation.value.onfinish = () => { onAnimationFinish(true) }
-        // If the.valueanimation is cancelled, isExpanding variable is set to false
-        animation.value.oncancel = () => isExpanding.value = false
-      }
+          // Start a WAAPI animation
+          animation.value = el.animate({
+            // Set the keyframes from the startHeight to endHeight
+            height: [startHeight, endHeight],
+          }, {
+            duration: 400,
+            easing: "ease-in-out",
+          })
+          // When the animation is complete, call onAnimationFinish()
+          animation.value.onfinish = () => { onAnimationFinish(true) }
+          // If the.valueanimation is cancelled, isExpanding variable is set to false
+          animation.value.oncancel = () => isExpanding.value = false
+        }
 
-      function onAnimationFinish(open: boolean) {
-        // Set the open attribute based on the parameter
-        el.open = open
-        // Clear the stored animation
-        animation.value = undefined
-        // Reset isClosing & isExpanding
-        isClosing.value = false
-        isExpanding.value = false
-        // Remove the overflow hidden and the fixed height
-        el.style.height = el.style.overflow = ""
-      }
-    })
-  }
-
+        function onAnimationFinish(open: boolean) {
+          // Set the open attribute based on the parameter
+          el.open = open
+          // Clear the stored animation
+          animation.value = undefined
+          // Reset isClosing & isExpanding
+          isClosing.value = false
+          isExpanding.value = false
+          // Remove the overflow hidden and the fixed height
+          el.style.height = el.style.overflow = ""
+        }
+      })
+    }
+  }, 500)
 }
 
 
